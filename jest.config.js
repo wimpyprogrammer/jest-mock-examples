ed950a6c409399622dc408a23551fb2f302f0b20
Drew Keller <drew@wimpyprogrammer.com>
2021-07-22 17:46:16 -0500

    Fix Jest config for properties not inherited

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
-module.exports = {
+const baseConfig = {
 	testEnvironment: 'node',
+
 	// Prevent tests from affecting each other
 	restoreMocks: true,
+};
+
+module.exports = {
 	verbose: true,
 	projects: [
 		{
+			...baseConfig,
 			displayName: 'Automock Enabled',
 			testMatch: ['<rootDir>/test/automock-on/*.test.js'],
 			automock: true,
 		},
 		{
+			...baseConfig,
 			displayName: 'Automock Disabled',
 			testMatch: ['<rootDir>/test/automock-off/*.test.js'],
 		},
